64664ee67218a31d7336bb5d56db478021dbecbd
Warren Smith <warren@wandrsmith.net>
2020-09-24 15:53:03 -0500

    Delete unused serializer module.

--- spacex_api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import CharField, Serializer
-
-
-class LaunchpadSerializer(Serializer):
-    id = CharField()
-    name = CharField(source='full_name')
-    status = CharField()
